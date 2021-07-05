565989373f6f09235fc2fcaff805766b73ff0781
tiagorahal <rahal.aires@gmail.com>
2021-07-05 18:36:41 -0300

    Remove makeArray function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,16 @@
 let bookArray = [];
 let myStorage = window.localStorage;
+bookArray = Object.entries(myStorage);
 
 function addBooks() {
   let bookName = document.getElementById('name-book').value;
   let bookAuthor = document.getElementById('author-book').value;
   localStorage.setItem(bookName, bookAuthor)
+
 }
 
 function removeBooks(book) {
   localStorage.removeItem(book);
 }
-
-function makeArray() {
-  bookArray = Object.entries(myStorage);
-  
-}
-
-makeArray()
 
 console.log(bookArray);
