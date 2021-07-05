54fa4cb6c42f3cc40c5f7d9b257f220f6ba8a742
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-07-05 15:35:55 -0500

    Add the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,10 @@ let bookArray = [];
 function addBooks() {
   let bookName = document.getElementById('name-book').value;
   let bookAuthor = document.getElementById('author-book').value;
-  let bookObject = {name: bookName , author: bookAuthor};
-  
-  bookArray.push(bookObject);
-  console.log(bookArray);
+  localStorage.setItem(bookName, bookAuthor)
 }
 
+myStorage = window.localStorage;
 
+alert(myStorage)
 
