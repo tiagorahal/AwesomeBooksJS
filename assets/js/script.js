3b3c9a294e5ca5441f593c6dd2064249e526038c
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-07-06 16:09:30 -0500

    Put the Addbooks function inside the class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,41 @@
-let bookArray = [];
-const html = [];
-const myStorage = window.localStorage;
-bookArray = Object.entries(myStorage);
+class Book {
+  constructor() {
+    this.bookarray = Object.entries(window.localStorage);
+    this.html = [];
+  }
+
+  addBooks() {
+    this.bookName = document.getElementById('name-book').value;
+    this.bookAuthor = document.getElementById('author-book').value;
+    localStorage.setItem(this.bookName, this.bookAuthor);
+  }
+
+  
+
+}
+
+const book = new Book();
+
+// let bookArray = [];
+// const html = [];
+// bookArray = Object.entries(window.localStorage);
 
 // eslint-disable-next-line no-unused-vars
-function addBooks() {
-  const bookName = document.getElementById('name-book').value;
-  const bookAuthor = document.getElementById('author-book').value;
-  localStorage.setItem(bookName, bookAuthor);
-}
+// function addBooks() {
+//   const bookName = document.getElementById('name-book').value;
+//   const bookAuthor = document.getElementById('author-book').value;
+//   localStorage.setItem(bookName, bookAuthor);
+// }
 
 function removeBooks(i) {
   const title = i.firstChild.innerText;
   Object.fromEntries(bookArray);
   localStorage.removeItem(title);
-  bookArray = Object.entries(myStorage);
+  bookArray = Object.entries(window.localStorage);
   i.remove();
 }
+
+
 
 bookArray.forEach(([key, value]) => {
   html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove'>Remove</button><br><br></div>`);
@@ -28,3 +47,5 @@ for (let i = 0; i < document.getElementsByClassName('remove').length; i += 1) {
   const bookTitle = document.getElementsByClassName('remove')[i].parentElement;
   document.getElementsByClassName('remove')[i].addEventListener('click', () => { removeBooks(bookTitle); });
 }
+
+
