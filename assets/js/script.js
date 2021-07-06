6112331c80bc5befa6c9a1680f7f85ce8d0531bb
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-07-06 09:26:43 -0500

    Fix linter errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const html = [];
 const myStorage = window.localStorage;
 bookArray = Object.entries(myStorage);
 
+// eslint-disable-next-line no-unused-vars
 function addBooks() {
   const bookName = document.getElementById('name-book').value;
   const bookAuthor = document.getElementById('author-book').value;
@@ -17,13 +18,13 @@ function removeBooks(i) {
   i.remove();
 }
 
-const result = bookArray.forEach(([key, value]) => {
-  html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p>` + '<button class=\'remove\'>Remove</button><br><br></div>');
+bookArray.forEach(([key, value]) => {
+  html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove'>Remove</button><br><br></div>`);
 });
 
 document.getElementById('book-list').innerHTML = html.join(' ');
 
-for (let i = 0; i < document.getElementsByClassName('remove').length; i++) {
+for (let i = 0; i < document.getElementsByClassName('remove').length; i += 1) {
   const bookTitle = document.getElementsByClassName('remove')[i].parentElement;
   document.getElementsByClassName('remove')[i].addEventListener('click', () => { removeBooks(bookTitle); });
 }
