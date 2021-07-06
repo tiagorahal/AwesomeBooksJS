f752b5f6812963a5586b349a66bada01fdf2c474
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-07-06 17:26:53 -0500

    Add the remove function to the class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 class Book {
   constructor() {
-    this.bookArray = [];
     this.bookArray = Object.entries(window.localStorage);
     this.html = [];
   }
@@ -13,36 +12,24 @@ class Book {
 
   display() {
     this.bookArray.forEach(([key, value]) => {
-      this.html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove'>Remove</button><br><br></div>`);
+      this.html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove' onclick="book.removeBooks('${key}')">Remove</button><br><br></div>`);
     });
     document.getElementById('book-list').innerHTML = this.html.join(' ');
   }
 
-
+  removeBooks(i) {
+    localStorage.removeItem(i);
+    this.bookArray = Object.entries(window.localStorage);
+    location.reload();
+  }
 }
 
 const book = new Book();
 book.display();
 
-function removeBooks(i) {
-  const title = i.firstChild.innerText;
-  Object.fromEntries(bookArray);
-  localStorage.removeItem(title);
-  bookArray = Object.entries(window.localStorage);
-  i.remove();
-}
-
-
-
-// bookArray.forEach(([key, value]) => {
-//   html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove'>Remove</button><br><br></div>`);
-// });
-
-// document.getElementById('book-list').innerHTML = html.join(' ');
-
-for (let i = 0; i < document.getElementsByClassName('remove').length; i += 1) {
-  const bookTitle = document.getElementsByClassName('remove')[i].parentElement;
-  document.getElementsByClassName('remove')[i].addEventListener('click', () => { removeBooks(bookTitle); });
-}
-
-
+// function removeBooks(i) {
+//   let titlebook = String(i);
+//   localStorage.removeItem(titlebook);
+//   book.bookArray = Object.entries(window.localStorage);
+//   location.reload();
+// }
