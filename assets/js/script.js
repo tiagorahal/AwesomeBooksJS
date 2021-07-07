1176476e054304f6aa30b9ae537f81c2420e87d5
tiagorahal <rahal.aires@gmail.com>
2021-07-07 18:21:55 -0300

    Finish book list section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ class Book {
 
   display() {
     this.bookArray.forEach(([key, value]) => {
-      this.html.push(`<div class='book d-flex col-12 p-2'><div class='d-flex col-10 text-container'><p class='book-title m-0'>${key} by</p><p class='book-author m-0'>&nbsp;${value}</p></div><button class='remove' onclick="book.removeBooks('${key}')">Remove</button></div>`);
+      this.html.push(`<div class='book d-flex col-12 p-2'><div class='d-flex col-9 col-md-10 text-container'><p class='book-title m-0'>${key} by</p><p class='book-author m-0'>&nbsp;${value}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
     });
     document.getElementById('book-list').innerHTML = this.html.join(' ');
   }
